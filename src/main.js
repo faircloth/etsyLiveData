97ef6a7f4c6ddc833d74b9147a07dde663502534
Andrew Faircloth <andrewkfaircloth@gmail.com>
2015-10-12 13:06:25 -0400

    copied over main content

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 (function () {
 
-  console.log('It Works!');
+  var templateString = $('#itemTemplate').html();
+
+  var templateFunction = _.template(templateString);
+
+  var firstTwentyFive = _.first(etsy.results,25);
+
+  _.each(firstTwentyFive, function (item) {
+  // console.log(item);
+  var itemHTML = templateFunction(item);
+  $('.itemBoxes').append(itemHTML);
+});
+
+
+  $('#hambutton').on('click', '#hambutton', function (event){
+  var btn = $(event.target);
+  var popup = $(btn.sibling('#popup'));
+  popup.removeClass('hidden');
+  popup.addClass('reveallist');
+});
+
 
 }());
